e4c48bb57565d9fb0441d5dcc7668738e2ffa262
John Griffiths <43955633+Laserbase@users.noreply.github.com>
2021-08-10 21:58:15 +0100

    remove all green

--- app/Models/StorageBin.php
+++ app/Models/StorageBin.php
@@ -25,6 +25,7 @@ class StorageBin extends Model
     private string $product_id = "";
     private int $level = 0;
     private $control = [];
+    private $overflow = 0;
     /**
      *     private int $quantity = 0; 
      *     private decimal $cost = 0.00;
@@ -41,9 +42,6 @@ class StorageBin extends Model
 
         $this->add($product_id, $quantity, $cost, $date_purchased);
     }
-    public function getStatus() {
-        return ["product_id" => $this->product_id, "level" => $this->level];
-    }
     public function add(string $product_id, int $quantity, float $cost, string $date_purchased)
     {
         $this->check_quantity($quantity)
@@ -54,27 +52,91 @@ class StorageBin extends Model
         }
 
         $this->level += $quantity;
-        $control[] = ["quantity" => $quantity, "cost" => $cost, "date" => $date_purchased];
+        $this->control[] = ["quantity" => $quantity, "cost" => $cost, "date" => $date_purchased];
     }
+
     public function remove(string $product_id, int $quantity) : int
     {
         $this->check_quantity($quantity)
-            ->check_product_id($product_id);
+            ->check_product_id($product_id)
+            ->check_stock_level($product_id, $quantity);
+        
+        $quantity_to_remove = $quantity;
+        foreach ($this->control as $key => $control) {
+            if ($quantity_to_remove < 1) {
+                break; // already complete
+            }
 
-        if ($this->level === 0) {
-            throw new StorageException("Unable to remove '{$quantity}' from '{$product_id}' as the stock level is 0 {zero)");
+            $quantity_available = $control['quantity'];
+            if ($quantity_available < 1) {
+                continue; // already picked
+            }
+
+            if ($quantity_to_remove > $quantity_available) {
+                $quantity_to_remove -= $quantity_available;
+            } else {
+                $quantity_available = $quantity_to_remove;
+                $quantity_to_remove = 0;
+            }
+
+            $this->level -= $quantity_available;
+            $this->control[$key]['quantity'] -= $quantity_available;
+
+        
         }
 
-        $result = 0;
-        if ($quantity > $this->level) {
-            $result = abs($this->level - $quantity);
-            $this->level = 0;
-        } else {
-            $this->level -= $quantity;
-        }
-    
-        return $result;
+        return $this->overflow = $quantity_to_remove;
     }
+    public function move(StorageBin $to_bin, int $quantity) : int
+    {
+        $product_id = $this->product_id;
+        $to_product_id = $to_bin->getStatus()['product_id'];
+        if ($product_id !== $to_product_id) {
+            throw new StorageException("Unable to move '{$quantity}' of '{$product_id}' to '{$to_product_id}', they must be the same product");
+        }
+
+        $this->check_quantity($quantity)
+            ->check_stock_level($product_id, $quantity);
+
+        $quantity_to_move = $quantity;
+        foreach ($this->control as $key => $control) {
+            if ($quantity_to_move < 1) {
+                break;
+            }
+
+            $quantity_available = $control['quantity'];
+            if ($quantity_available < 1) {
+                continue; // already picked
+            }
+
+            if ($quantity_to_move > $quantity_available) {
+                $quantity_to_move -= $quantity_available;
+            } else {
+                $quantity_available = $quantity_to_move;
+                $quantity_to_move = 0;
+            }
+
+            $this->level -= $quantity_available;
+            $this->control[$key]['quantity'] -= $quantity_available;
+            
+            $cost = $control['cost'];
+            $date = $control['date'];
+            $to_bin->add($product_id, $quantity_available, $cost, $date);
+        }
+
+        return $this->overflow = $quantity_to_move;
+    }
+
+    public function getStatus() {
+        return [
+            "product_id" => $this->product_id, 
+            "level" => $this->level, 
+            "control" => $this->control,
+            "overflow" => $this->overflow
+        ];
+    }
+
+    //---
     private function check_product_id(string $product_id)
     {
         if ($this->product_id !== $product_id) {
@@ -90,5 +152,19 @@ class StorageBin extends Model
         }
 
         return $this;
+    }
+    private function check_stock_level(string $product_id, int $quantity)
+    {
+        if ($this->level === 0) {
+            throw new StorageException("Unable to remove '{$quantity}' from '{$product_id}' as the stock level is 0 {zero)");
+        }
+
+    }
+    public function log(string $msg, int $line = 0)
+    {
+        \file_put_contents("/Users/johng/src/freetimers/error_log", 
+            "[".date('Y-m-d h:i:s')."] '{$msg}}' LINE='{$line}'\n",
+            FILE_APPEND
+        );
     }
 }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -18,19 +18,21 @@ class FreetimersTest extends TestCase
         $this->assertTrue(true);
     }
 
+    //--- create ---
     public function test_starage_bin_exists()
     {   // string $product_id, int $quantity, float $cost, string $date_purchased
         $bin = new \App\Models\StorageBin("bolts", 10, 10.00, date("2021-01-01"));
         $this->assertIsObject($bin);
     }
 
-    public function test_can_add_stock()
+    public function test_cannot_create_with_zero_stock()
     {
         $this->expectException(\app\Models\StorageException::class);
-        $bin = new \App\Models\StorageBin("bolts", 10, 10.00, date("2021-01-01"));
+        $bin = new \App\Models\StorageBin("bolts", 0, 10.00, date("2021-01-01"));
         $bin->add("screws", 10, 10.99, date("2021-01-01"));
     }
 
+    //--- add ---
     public function test_adding_zero_stock()
     {
         $this->expectException(\app\Models\StorageException::class);
@@ -44,29 +46,32 @@ class FreetimersTest extends TestCase
         $bin->add("bolts", -10, 10.99, date("2021-01-01"));
     }
 
+    //--- remove ---
     public function test_removing_stock()
     {
         $bin = new \App\Models\StorageBin("bolts", 3, 10.00, date("2021-01-01"));
         $status = $bin->getStatus();
+        //$bin->log("status='".print_r($status, true)."'", __LINE__);
         $this->assertTrue($status["level"] === 3);
 
         $overflow = $bin->remove("bolts", 1);
         $status = $bin->getStatus();
+        //$bin->log("status='".print_r($status, true)."'", __LINE__);
         $this->assertTrue($overflow === 0); // 2
         $this->assertTrue($status["level"] === 2);
 
         $overflow = $bin->remove("bolts", 1);
         $status = $bin->getStatus();
+        //$bin->log("status='".print_r($status, true)."'", __LINE__);
         $this->assertTrue($overflow === 0); // 1
         $this->assertTrue($status["level"] === 1);
 
         $overflow = $bin->remove("bolts", 1);
         $this->assertTrue($overflow === 0); // error
-        print_r($bin->getStatus());
 
         $this->expectException(\app\Models\StorageException::class);
         $overflow = $bin->remove("bolts", 1);
-//        $this->assertTrue($overflow === 0);
+        $this->assertTrue($overflow === 0);
 
     }
 
@@ -74,15 +79,18 @@ class FreetimersTest extends TestCase
     {
         $bin = new \App\Models\StorageBin("bolts", 10, 10.00, date("2021-01-01"));
         $status = $bin->getStatus();
+        //$bin->log("status='".print_r($status, true)."'", __LINE__);
         $this->assertTrue($status["level"] === 10);
 
         $overflow = $bin->remove("bolts", 5);
         $status = $bin->getStatus();
+        //$bin->log("status='".print_r($status, true)."'", __LINE__);
         $this->assertTrue($overflow === 0);
         $this->assertTrue($status["level"] === 5);
 
         $overflow = $bin->remove("bolts", 6);
         $status = $bin->getStatus();
+        //$bin->log("status='".print_r($status, true)."'", __LINE__);
         $this->assertTrue($overflow === 1); 
         $this->assertTrue($status["level"] === 0);
 
@@ -130,14 +138,118 @@ class FreetimersTest extends TestCase
         //---
     }
 
-    public function test_moving_stock()
+    //--- move ---
+    public function test_moving_stock_different_products()
+    {
+        $this->expectException(\app\Models\StorageException::class);
+        //$this->expectedExceptionMessage('xxx');
+
+        $bolts = new \App\Models\StorageBin("bolts", 10, 1.00, date("2021-01-01"));
+        $screws = new \App\Models\StorageBin("screws", 10, 1.00, date("2021-01-01"));
+
+        $overflow = $bolts->move($screws, 2);
+    }
+    public function test_moving_zero_stock()
     {
         $this->expectException(\app\Models\StorageException::class);
 
-        $bolts = new \App\Models\StorageBin("bolts", 10, 10.00, date("2021-01-01"));
-        $screws = new \App\Models\StorageBin("screws", 1, 1.00, date("2021-01-01"));
-        $overflow = Bolts->move($screws, 1);
-        
-        $this->assertTrue(false);
+        $bolts1 = new \App\Models\StorageBin("bolts", 10, 0.00, date("2021-01-01"));
+        $bolts2 = new \App\Models\StorageBin("bolts", 10, 1.00, date("2021-01-01"));
+
+        $overflow = $bolts1->move($bolts2, 0);
     }
+    public function test_moving_same_stock()
+    {
+        $bolts1 = new \App\Models\StorageBin("bolts", 1, 1.00, date("2021-01-01"));
+        $bolts1->add("bolts", 9, 10.00, date("2021-02-02"));
+        $status1 = $bolts1->getStatus();
+        $this->assertTrue($status1["level"] === 10);
+
+        $bolts2 = new \App\Models\StorageBin("bolts", 1, 1.00, date("2021-01-01"));
+        $bolts2->add("bolts", 9, 10.00, date("2021-02-02"));
+        $status2 = $bolts2->getStatus();
+        $this->assertTrue($status2["level"] === 10);
+
+        $overflow = $bolts1->move($bolts2, 9);
+        $status1 = $bolts1->getStatus();
+        $status2 = $bolts2->getStatus();
+        //$bolts1->log("bolts1='".print_r($status1, true)."'", __LINE__);
+        //$bolts2->log("bolts2='".print_r($status2, true)."'", __LINE__);
+
+        $this->assertTrue($status1['level'] === 1);
+        $this->assertTrue($status2['level'] === 19);
+    }
+    public function test_moving_same_stock_boundary()
+    {
+        $bolts1 = new \App\Models\StorageBin("bolts", 1, 1.00, date("2021-01-01"));
+        $bolts1->add("bolts", 2, 10.00, date("2021-02-02"));
+        $status1 = $bolts1->getStatus();
+        $this->assertTrue($status1["level"] === 3);
+
+        $bolts2 = new \App\Models\StorageBin("bolts", 1, 1.00, date("2021-01-01"));
+        //$bolts2->add("bolts", 9, 10.00, date("2021-02-02"));
+        $status2 = $bolts2->getStatus();
+        $this->assertTrue($status2["level"] === 1);
+
+        $overflow = $bolts1->move($bolts2, 1);
+        $status1 = $bolts1->getStatus();
+        $status2 = $bolts2->getStatus();
+        $this->assertTrue($overflow === 0);
+        $this->assertTrue($status1['level'] === 2);
+        $this->assertTrue($status2['level'] === 2);
+
+        $overflow = $bolts1->move($bolts2, 1);
+        $status1 = $bolts1->getStatus();
+        $status2 = $bolts2->getStatus();
+        $this->assertTrue($overflow === 0);
+        $this->assertTrue($status1['level'] === 1);
+        $this->assertTrue($status2['level'] === 3);
+
+        $overflow = $bolts1->move($bolts2, 1);
+        $status1 = $bolts1->getStatus();
+        $status2 = $bolts2->getStatus();
+        $this->assertTrue($overflow === 0);
+        $this->assertTrue($status1['level'] === 0);
+        $this->assertTrue($status2['level'] === 4);
+
+        $this->expectException(\app\Models\StorageException::class);
+        $overflow = $bolts1->move($bolts2, 1);
+        $status1 = $bolts1->getStatus();
+        $status2 = $bolts2->getStatus();
+        $this->assertTrue($overflow === 1);
+        $this->assertTrue($status1['level'] === 0);
+        $this->assertTrue($status2['level'] === 4);
+    }
+    public function test_moving_same_stock_boundary_overflow()
+    {
+        $bolts1 = new \App\Models\StorageBin("bolts", 1, 1.00, date("2021-01-01"));
+        $status1 = $bolts1->getStatus();
+        $this->assertTrue($status1["overflow"] === 0);
+        $this->assertTrue($status1["level"] === 1);
+
+        $bolts1->add("bolts", 9, 10.00, date("2021-02-02"));
+        $status1 = $bolts1->getStatus();
+        $this->assertTrue($status1["overflow"] === 0);
+        $this->assertTrue($status1["level"] === 10);
+
+        $bolts2 = new \App\Models\StorageBin("bolts", 1, 1.00, date("2021-01-01"));
+        $status2 = $bolts2->getStatus();
+        $this->assertTrue($status2["overflow"] === 0);
+        $this->assertTrue($status2["level"] === 1);
+
+        $overflow = $bolts1->move($bolts2, 20);
+        $this->assertTrue($overflow === 10);
+
+        $status1 = $bolts1->getStatus();
+        $this->assertTrue($status1["overflow"] === $overflow);
+        $this->assertTrue($status1['level'] === 0);
+
+        $status2 = $bolts2->getStatus();
+        $this->assertTrue($status2["overflow"] === 0);
+        $this->assertTrue($status2['level'] === 11);
+
+        //$bolts1->log("overflow='".print_r($overflow, true)."'", __LINE__);
+        //$bolts1->log("bolts1='".print_r($bolts1, true)."'", __LINE__);
+    }
+
 }
